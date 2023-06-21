8bcdb61a37000677c2c44e355a74889804a4ff20
Christoph Hellmann Santos <christoph.hellmann.santos@ipa.fraunhofer.de>
2023-06-21 09:34:18 +0200

    Revert "Trimmed the branch to only the canopen_interfaces sub directory"
    This reverts commit 7cf86b9c4ae95cc50631bba3ae93ef63c9ba9912.

---
+++ canopen_tests/launch_tests/test_proxy_driver.py
@@ -0,0 +1,109 @@
+#    Copyright 2022 Christoph Hellmann Santos
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import os
+from time import sleep
+import pytest
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+import launch
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import launch_testing
+import threading
+import rclpy
+from rclpy.executors import ExternalShutdownException
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
+from canopen_utils.test_node import TestNode
+from lifecycle_msgs.msg import State, Transition
+from std_srvs.srv import Trigger
+import unittest
+
+
+@pytest.mark.rostest
+def generate_test_description():
+
+    launch_desc = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory("canopen_tests"), "launch"),
+                "/proxy_setup.launch.py",
+            ]
+        )
+    )
+
+    ready_to_test = launch.actions.TimerAction(
+        period=5.0,
+        actions=[launch_testing.actions.ReadyToTest()],
+    )
+
+    return (LaunchDescription([launch_desc, ready_to_test]), {})
+
+
+class TestLifecycle(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("SetupClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("TearDownClass")
+
+    @classmethod
+    def setUp(self):
+        print("Setup")
+        rclpy.init()
+        self.node = TestNode()
+        self.x = threading.Thread(target=rclpy.spin, args=[self.node])
+        self.x.start()
+
+    def tearDown(self):
+        print("TearDown")
+        rclpy.shutdown()
+        self.node.destroy_node()
+        self.x.join()
+
+    def test_02_nmt(self):
+        assert self.node.checkTrigger("proxy_device_1", "nmt_reset_node")
+        assert self.node.checkTrigger("proxy_device_2", "nmt_reset_node")
+        sleep(1.0)
+
+    def test_03_sdo_read(self):
+        assert self.node.checkSDORead("proxy_device_1", index=0x1000, subindex=0, data=0)
+        assert self.node.checkSDORead("proxy_device_2", index=0x1000, subindex=0, data=0)
+
+    def test_04_sdo_write(self):
+        assert self.node.checkSDOWrite("proxy_device_1", index=0x4000, subindex=0, data=100)
+        assert self.node.checkSDOWrite("proxy_device_2", index=0x4000, subindex=0, data=100)
+        assert self.node.checkSDORead("proxy_device_1", index=0x4000, subindex=0, data=100)
+        assert self.node.checkSDORead("proxy_device_2", index=0x4000, subindex=0, data=100)
+
+    def test_05_sdo_read_id(self):
+        assert self.node.checkSDOReadID(node_id=2, index=0x4000, subindex=0, type=0x7, data=100)
+        assert self.node.checkSDOReadID(node_id=3, index=0x4000, subindex=0, type=0x7, data=100)
+
+    def test_06_sdo_write_id(self):
+        assert self.node.checkSDOWriteID(node_id=2, index=0x4000, subindex=0, type=0x7, data=999)
+        assert self.node.checkSDOWriteID(node_id=3, index=0x4000, subindex=0, type=0x7, data=999)
+        assert self.node.checkSDOReadID(node_id=2, index=0x4000, subindex=0, type=0x7, data=999)
+        assert self.node.checkSDOReadID(node_id=3, index=0x4000, subindex=0, type=0x7, data=999)
+
+    # def test_07_rpdo_tpdo(self):
+    #     assert self.node.checkRpdoTpdo(
+    #         "proxy_device_1", index=0x4000, subindex=0, data=101
+    #     )
+    #     assert self.node.checkRpdoTpdo(
+    #         "proxy_device_2", index=0x4000, subindex=0, data=202
+    #     )
